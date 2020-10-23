4861fd98e5fd1c42ab84d2e3b67eb793604bbe66
nhoknecz01 <73054636+nhoknecz01@users.noreply.github.com>
2020-10-23 10:37:00 +0700

    Tri
    Update source setting role

--- src/main/java/com/example/demo/reponsitory/RoleRepository.java
+++ src/main/java/com/example/demo/reponsitory/RoleRepository.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import com.example.demo.model.role.Role;
 import com.example.demo.model.role.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.w3c.dom.stylesheets.LinkStyle;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(RoleName name);
